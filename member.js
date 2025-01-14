c5884facf9534f15b4e7f6d06f5e7765f8544f75
Harmin <harmin1905@gmail.com>
2025-01-14 04:05:35 +0000

    Add method using multiple suggestions

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'Harmin Arora',
+        age: 30,
+        skills: ['JavaScript', 'Node.js', 'React', 'MongoDB']
+    };
+
+    return member;
+}
